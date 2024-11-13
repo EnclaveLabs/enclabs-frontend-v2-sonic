313a1218b7bb049dc57d5325c62a723103049b4e
KageDeFi <kagedefi@proton.me>
2024-11-13 17:25:40 +0100

    [test]

--- apps/evm/src/scripts/fetchGraphInfo.ts
+++ apps/evm/src/scripts/fetchGraphInfo.ts
@@ -1,85 +1,85 @@
 require('dotenv').config();
-const { createClient } = require('@supabase/supabase-js');
+// const { createClient } = require('@supabase/supabase-js');
 
-const GRAPHQL_ENDPOINT = 'https://api.studio.thegraph.com/query/91097/enclabs-arb-test/version/latest';
+// const GRAPHQL_ENDPOINT = 'https://api.studio.thegraph.com/query/91097/enclabs-arb-test/version/latest';
 
-const supabase = createClient(process.env.DB_URL, process.env.DB_API_KEY)
+// const supabase = createClient(process.env.DB_URL, process.env.DB_API_KEY)
 
-// Data in DB
-type HistoryData = {
-    created_at?: string;
-    block_number: string;
-    block_timestamp: string;
-    borrow_apy: string;
-    supply_apy: string;
-    total_borrow_cents: string;
-    total_supply_cents: string;
-    market_address: string;
-};
+// // Data in DB
+// type HistoryData = {
+//     created_at?: string;
+//     block_number: string;
+//     block_timestamp: string;
+//     borrow_apy: string;
+//     supply_apy: string;
+//     total_borrow_cents: string;
+//     total_supply_cents: string;
+//     market_address: string;
+// };
 
-// Data from Subgraph
-type FetchedMarketData = {
-    id: string;
-    name: string;
-    symbol: string;
-    accrualBlockNumber: number;
-    blockTimestamp: number;
-    borrowRateMantissa: string;
-    supplyRateMantissa: string;
-    totalBorrowsMantissa: string;
-    totalSupplyVTokenMantissa: string;
-};
+// // Data from Subgraph
+// type FetchedMarketData = {
+//     id: string;
+//     name: string;
+//     symbol: string;
+//     accrualBlockNumber: number;
+//     blockTimestamp: number;
+//     borrowRateMantissa: string;
+//     supplyRateMantissa: string;
+//     totalBorrowsMantissa: string;
+//     totalSupplyVTokenMantissa: string;
+// };
 
-async function fetchGraphQLData() {
+// async function fetchGraphQLData() {
 
-    const graphQuery = 
-    `query {
-        markets {
-            id
-            name
-            symbol
-            accrualBlockNumber
-            blockTimestamp
-            borrowRateMantissa
-            supplyRateMantissa
-            totalBorrowsMantissa
-            totalSupplyVTokenMantissa
-        }
-    }`;
+//     const graphQuery = 
+//     `query {
+//         markets {
+//             id
+//             name
+//             symbol
+//             accrualBlockNumber
+//             blockTimestamp
+//             borrowRateMantissa
+//             supplyRateMantissa
+//             totalBorrowsMantissa
+//             totalSupplyVTokenMantissa
+//         }
+//     }`;
 
-    const response = await fetch(GRAPHQL_ENDPOINT, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ query: graphQuery })
-    });
+//     const response = await fetch(GRAPHQL_ENDPOINT, {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({ query: graphQuery })
+//     });
 
-    const data = await response.json();
-    return data.data;
-}
+//     const data = await response.json();
+//     return data.data;
+// }
 
-async function saveDataToSupabase(markets: FetchedMarketData[]) {
+// async function saveDataToSupabase(markets: FetchedMarketData[]) {
 
-    const historyMarket: HistoryData[] = markets.map(market => ({
-        market_address: market.id,
-        block_number: market.accrualBlockNumber.toString(),
-        block_timestamp: market.blockTimestamp.toString(),
-        borrow_apy: '1',
-        supply_apy: '2',
-        total_borrow_cents: market.totalBorrowsMantissa,
-        total_supply_cents: market.totalSupplyVTokenMantissa,
-    }));
+//     const historyMarket: HistoryData[] = markets.map(market => ({
+//         market_address: market.id,
+//         block_number: market.accrualBlockNumber.toString(),
+//         block_timestamp: market.blockTimestamp.toString(),
+//         borrow_apy: '1',
+//         supply_apy: '2',
+//         total_borrow_cents: market.totalBorrowsMantissa,
+//         total_supply_cents: market.totalSupplyVTokenMantissa,
+//     }));
 
-    // Save in Database
-    const { data, error } = await supabase
-        .from('History')
-        .insert(historyMarket);
+//     // Save in Database
+//     const { data, error } = await supabase
+//         .from('History')
+//         .insert(historyMarket);
 
-    if (error) {
-        console.error('Error when saving data: ', error);
-    } else {
-        console.log('Data saved successfully: ', data);
-    }
-}
+//     if (error) {
+//         console.error('Error when saving data: ', error);
+//     } else {
+//         console.log('Data saved successfully: ', data);
+//     }
+// }
 
 async function run() {
     // const data = await fetchGraphQLData();
