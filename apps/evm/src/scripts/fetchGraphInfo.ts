4590166c98d534862952851c613d654de389bf98
KageDeFi <kagedefi@proton.me>
2024-11-15 12:16:58 +0100

    [script] add new info in script market history

--- apps/evm/src/scripts/fetchGraphInfo.ts
+++ apps/evm/src/scripts/fetchGraphInfo.ts
@@ -1,20 +1,28 @@
 require('dotenv').config();
+const BigNumber = require('bignumber.js');
 const { createClient } = require('@supabase/supabase-js');
 
 const GRAPHQL_ENDPOINT = 'https://api.studio.thegraph.com/query/91097/enclabs-arb-test/version/latest';
 
 const supabase = createClient(process.env.DB_URL, process.env.DB_API_KEY)
+const COMPOUND_DECIMALS = 18;
+const DAYS_PER_YEAR = 365;
+const SECONDS_PER_DAY = 60 * 60 * 24;
 
 // Data in DB
 type HistoryData = {
-    created_at?: string;
+    market_name: string;
+    market_address: string;
     block_number: string;
     block_timestamp: string;
-    borrow_apy: string;
     supply_apy: string;
-    total_borrow_cents: string;
-    total_supply_cents: string;
-    market_address: string;
+    total_supply_token: string;
+    total_supply_price_usd: string;
+    borrow_apy: string;
+    total_borrow_token: string;
+    total_borrow_price_usd: string;
+    total_liquidity_token: string;
+    total_liquidity_price_usd: string;
 };
 
 // Data from Subgraph
@@ -24,10 +32,23 @@ type FetchedMarketData = {
     symbol: string;
     accrualBlockNumber: number;
     blockTimestamp: number;
-    borrowRateMantissa: string;
     supplyRateMantissa: string;
-    totalBorrowsMantissa: string;
     totalSupplyVTokenMantissa: string;
+    borrowRateMantissa: string;
+    totalBorrowsMantissa: string;
+    cashMantissa: string;
+    underlyingPriceCentsMantissa: string;
+    underlyingDecimals:number;
+    vTokenDecimals: number;
+};
+
+type SupplyBorrowValues = {
+    totalSupplyTokens: string;
+    totalSupplyInUSD: string;
+    totalBorrowTokens: string;
+    totalBorrowInUSD: string;
+    totalLiquidityTokens: string;
+    totalLiquidityInUSD: string;
 };
 
 async function fetchGraphQLData() {
@@ -44,6 +65,10 @@ async function fetchGraphQLData() {
             supplyRateMantissa
             totalBorrowsMantissa
             totalSupplyVTokenMantissa
+            vTokenDecimals
+            underlyingPriceCentsMantissa
+            underlyingDecimals
+            cashMantissa
         }
     }`;
 
@@ -57,17 +82,78 @@ async function fetchGraphQLData() {
     return data.data;
 }
 
+
+const calculateAPY = (
+    rateMantissa: string,
+    decimals: number = COMPOUND_DECIMALS
+    ) =>
+    new BigNumber(rateMantissa)
+      // Convert mantissa to tokens
+      .div(10 ** decimals)
+      // Convert to daily rate
+      .multipliedBy(SECONDS_PER_DAY * DAYS_PER_YEAR)
+      .dp(decimals)
+      .multipliedBy(100)
+      .toString();
+
+const calculateSupplyBorrowValues = (
+    market: FetchedMarketData) : SupplyBorrowValues => {
+
+    // Price per token
+    const underlyingPriceUSD = new BigNumber(market.underlyingPriceCentsMantissa).dividedBy(10 ** COMPOUND_DECIMALS);
+  
+
+    // Total supplied tokens
+    const totalSupplyVToken = new BigNumber(market.totalSupplyVTokenMantissa).dividedBy(10 ** market.vTokenDecimals);
+    // Total supplied price
+    const totalSupplyInUSD = totalSupplyVToken.multipliedBy(underlyingPriceUSD).decimalPlaces(market.underlyingDecimals).toString();
+
+
+    // Total borrowed tokens
+    const totalBorrowVToken = new BigNumber(market.totalBorrowsMantissa).dividedBy(10 ** 18);
+    // Total borrowed price
+    const totalBorrowInUSD = totalBorrowVToken.multipliedBy(underlyingPriceUSD).decimalPlaces(market.underlyingDecimals).toString();
+
+
+    // Total liquidity price
+    const totalLiquidityToken = new BigNumber(market.cashMantissa).dividedBy(10 ** 18);
+    // Total liquidity price
+    const totalLiquidityInUSD = totalLiquidityToken.multipliedBy(underlyingPriceUSD).decimalPlaces(market.underlyingDecimals).toString();
+
+
+
+    const values: SupplyBorrowValues = {
+        totalSupplyTokens: totalSupplyVToken.toString(),
+        totalSupplyInUSD: totalSupplyInUSD,
+        totalBorrowTokens: totalBorrowVToken.toString(),
+        totalBorrowInUSD: totalBorrowInUSD,
+        totalLiquidityTokens: totalLiquidityToken.toString(),
+        totalLiquidityInUSD: totalLiquidityInUSD,
+    };
+    return values;
+}
+
 async function saveDataToSupabase(markets: FetchedMarketData[]) {
 
-    const historyMarket: HistoryData[] = markets.map(market => ({
-        market_address: market.id,
-        block_number: market.accrualBlockNumber.toString(),
-        block_timestamp: market.blockTimestamp.toString(),
-        borrow_apy: market.borrowRateMantissa,
-        supply_apy: market.supplyRateMantissa,
-        total_borrow_cents: market.totalBorrowsMantissa,
-        total_supply_cents: market.totalSupplyVTokenMantissa,
-    }));
+    const historyMarket: HistoryData[] = markets.map(market => {
+
+        const SupplyBorrowValues: SupplyBorrowValues = calculateSupplyBorrowValues(market);
+        
+        return {
+            market_name: market.name,
+            market_address: market.id,
+            block_number: market.accrualBlockNumber.toString(),
+            block_timestamp: market.blockTimestamp.toString(),
+            supply_apy: calculateAPY(market.supplyRateMantissa),
+            total_supply_token: SupplyBorrowValues.totalSupplyTokens,
+            total_supply_price_usd: SupplyBorrowValues.totalSupplyInUSD,
+            borrow_apy: calculateAPY(market.borrowRateMantissa),
+            total_borrow_token: SupplyBorrowValues.totalBorrowTokens,
+            total_borrow_price_usd: SupplyBorrowValues.totalBorrowInUSD,
+            total_liquidity_token: SupplyBorrowValues.totalLiquidityTokens,
+            total_liquidity_price_usd: SupplyBorrowValues.totalLiquidityInUSD
+        };
+    });
 
     // Save in Database
     const { data, error } = await supabase
@@ -89,5 +175,3 @@ async function run() {
 
 
 run();
-// Set up a cron job or setInterval here (example every 6 hours)
-// setInterval(run, 6 * 60 * 60 * 1000);
