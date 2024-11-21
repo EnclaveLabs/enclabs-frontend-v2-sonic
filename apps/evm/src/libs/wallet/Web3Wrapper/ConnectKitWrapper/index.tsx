8be378fec9751d4745f79d0a6505430e2d22d269
KageDeFi <kagedefi@proton.me>
2024-11-21 12:05:13 +0100

    [ui] update connectivity kit wagmi theme

--- apps/evm/src/libs/wallet/Web3Wrapper/ConnectKitWrapper/index.tsx
+++ apps/evm/src/libs/wallet/Web3Wrapper/ConnectKitWrapper/index.tsx
@@ -26,26 +26,52 @@ export const ConnectKitWrapper: React.FC<ConnectKitWrapperProps> = ({ children }
         '--ck-font-family': '"Proxima Nova Rg", var(--font-fallback)',
         '--ck-border-radius': '0.75rem',
         '--ck-overlay-background': 'rgba(0, 0, 0, 0.5)',
+
+        // Wallet buttons
+        '--ck-primary-button-box-shadow': '0px 8px 6px 0px rgba(0, 0, 0, 0.05)',
         '--ck-primary-button-border-radius': '0.5rem',
         '--ck-primary-button-hover-border-radius': '0.5rem',
         '--ck-primary-button-active-border-radius': '0.5rem',
-        '--ck-primary-button-background': theme.colors.blue,
-        '--ck-primary-button-hover-background': theme.colors.grey,
-        '--ck-primary-button-active-background': theme.colors.grey,
+        '--ck-primary-button-background': theme.colors.cards,
+        '--ck-primary-button-color': theme.colors.blue,
+        '--ck-primary-button-hover-color': theme.colors.white,
+        '--ck-primary-button-hover-background': theme.colors.blue,
+        '--ck-primary-button-active-background': theme.colors.blue,
+
+        // Copy to clipboard button
+        '--ck-secondary-button-background': theme.colors.blue,
+        '--ck-secondary-button-hover-background': theme.colors.mediumBlue,
+        '--ck-secondary-button-hover-color': theme.colors.white,
+        '--ck-secondary-button-active-background': theme.colors.white,
         '--ck-secondary-button-border-radius': '0.5rem',
         '--ck-secondary-button-hover-border-radius': '0.5rem',
         '--ck-secondary-button-active-border-radius': '0.5rem',
-        '--ck-secondary-button-background': theme.colors.blue,
-        '--ck-secondary-button-hover-background': theme.colors.grey,
-        '--ck-secondary-button-active-background': theme.colors.grey,
-        '--ck-body-action-color': theme.colors.grey,
-        '--ck-body-background-secondary': theme.colors.cards,
+
+        // Return and close button
+        '--ck-body-action-color': theme.colors.lightBlack,
+        '--ck-body-action-hover-color': theme.colors.lightBlack,
+
+        // Return and close button hover
+        '--ck-body-background-secondary': theme.colors.white,
+
+        // More available tooltip
         '--ck-tooltip-background': theme.colors.blue,
-        '--ck-tooltip-color': theme.colors.lightBlack,
-        '--ck-body-color-muted': theme.colors.grey,
+        '--ck-tooltip-color': theme.colors.white,
+
+        // Title + scrollbar
         '--ck-body-color': theme.colors.lightBlack,
+        '--ck-body-color-muted': theme.colors.lightBlack,
+        '--ck-body-divider': theme.colors.lightBlack,
+
+        // QR Code
+        '--ck-qr-dot-color': theme.colors.lightBlack,
+        '--ck-qr-border-color': theme.colors.lightBlack,
+        '--ck-qr-border-radius': '0.5rem',
+        
+
         '--ck-body-background-tertiary': theme.colors.blue,
-        '--ck-body-background': theme.colors.cards,
+
+        '--ck-body-background': theme.colors.background,
       }}
     >
       <AuthHandler />
