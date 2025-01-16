b4b5de11fe229a25fd7d81815254b955b9a580f0
KageDeFi <kagedefi@proton.me>
2025-01-16 20:38:53 +0100

    add token address for token type

--- apps/evm/src/constants/tokenType.ts
+++ apps/evm/src/constants/tokenType.ts
@@ -5,27 +5,15 @@ export const tokenType = {
          ""
     ],
     PT: [
-        "0xd1927b1FF6d2EE2Cb3A6B06AB4230726B4FFc816", // pt pump 26dec
-        "0x2161a68cD094F47a4f86E973ff5f4af7D80Bc78d", // pt solv 26dec
-        "0x2d16b3CC6b0E680e91b1A1788182Beb23ccc4078", // pt unibtc 26dec
-        "0xb83D10c4CF7b4f14E3A745a25C52619B344B0B2B", // pt LBTC 26dec
-        "0xe3248D189E8d02AF31B9516612fD855e959B54A5", // pt ebtc 26dec
-        "0xb33808ea0e883138680BA29311a220A7377cdb92", // pt weETH-26JUN2025
     ],
     Wrapped: [
-        "0xd04b37878C8dEEaEE4Ef38830e78F4BA22c7482D", // Wbtc
-        "0x82af49447d8a07e3bd95bd0d56f35241523fbab1", // WETH
-        "0xC024e43805aE16EC7bb8a572F4D4972489cfdc6E", // cbbtc
-        "0x5979D7b546E38E414F7E9822514be443A4800529", // wstETH
-        "0x35751007a407ca6FEFfE80b3cB397736D2cf4dbe", // weETH
+        "0x50c42deacd8fc9773493ed674b675be577f2634b", // wETH
+        "0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38", // wS
     ],
     aToken: [
-        "0xdf500703Bc7dbC89937B40d1142acb317B34648b", // aEthcbBTC
-        "0xccE9Fa110A04979eEd2E7eb51E50d6Dac3828227", // aEthWBTC
     ],
     Stable: [
-        "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9", // USDT
-        "0xaf88d065e77c8cc2239327c5edb3a432268e5831", // USDC
+        "0x29219dd400f2bf60e5a23d13be72b486d4038894", // USDC.e
     ],
 };
 
