79966632a46136d91645c816aa123631be5d225b
root <root@DESKTOP-OAV5STN.localdomain>
2025-01-22 21:59:20 +0100

    fix github link

--- apps/evm/src/containers/Layout/Footer/constants.ts
+++ apps/evm/src/containers/Layout/Footer/constants.ts
@@ -1,5 +1,5 @@
 export const VENUS_DISCORD_URL = 'https://discord.gg/AK2AzvydUz';
 export const VENUS_TWITTER_URL = 'https://x.com/EnclabsFi';
-export const VENUS_GITHUB_URL = 'https://github.com/EnclaveLabs';
+export const VENUS_GITHUB_URL = 'https://github.com/EnclabsTechIntern';
 export const VENUS_TELEGRAM_URL = 'https://t.me/EnclabsFinance';
 export const VENUS_DOC_URL = 'https://enclabs.gitbook.io/enclabs-documentation';
