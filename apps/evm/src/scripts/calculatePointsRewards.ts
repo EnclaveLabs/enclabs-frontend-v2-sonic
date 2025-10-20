ed8ddb7fac61d6d6ed071a9d83cfab602df0f962
root <root@DESKTOP-OAV5STN.localdomain>
2025-10-20 18:05:45 +0200

    fix negative points

--- apps/evm/src/scripts/calculatePointsRewards.ts
+++ apps/evm/src/scripts/calculatePointsRewards.ts
@@ -136,7 +136,7 @@ async function calculatePointsRewards(markets: Market[]) {
       //   BORROW_BONUS_MULTIPLICATOR
       // );
       const borrowPointsEarned = 0;
-      const supplyPointsEarned = Math.floor(estimatedDailySupplyPointsEarned / 4);
+      const supplyPointsEarned = Math.max(0,Math.floor(estimatedDailySupplyPointsEarned / 4));
       // Add cumulated points
       if (supplyPointsEarned > 0 || borrowPointsEarned > 0) {
         userPoints[acc.account.id] = (userPoints[acc.account.id] || 0) + supplyPointsEarned + borrowPointsEarned;
